daea05d8a6c269387818a448eb84fc1848108b30
maituduy <37800311+maituduy@users.noreply.github.com>
2018-04-03 19:22:25 +0700

    Update ConvertJson.java

--- src/ConvertJson.java
+++ src/ConvertJson.java
@@ -8,13 +8,16 @@ import java.util.*;
 public class ConvertJson {
 
     private static Gson gson = new GsonBuilder().create();
-
+    
+    // Truyền vào path, Lấy ra đc một List<Information> ra từ file JSON. 
+    // Từ list này có thể lấy ra được list client, cashPayment, creditPayment,resNumber 
     public static List<Information> getFromJSON(String path) throws FileNotFoundException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
         Type type = new TypeToken<List<Information>>(){}.getType();
         return gson.fromJson(bufferedReader, type);
     }
-
+    
+    // Để ghi dữ liệu ra file JSON cần truyền vào một List<Information>, path 
     public static void toJSON(List clazz, String path) {
         try {
             Writer writer = new FileWriter(path);
